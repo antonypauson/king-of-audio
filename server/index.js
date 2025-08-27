d53e0da9f1614015460bd9998b19d12e111f8373
antonypauson <13antonysun@gmail.com>
2025-08-27 10:30:28 +0530

    fix(server): localhost address to vercel address

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:8080", // socket server intialize
+        origin: ["http://localhost:8080", "https://king-of-audio.vercel.app"], // socket server intialize
         methods: ["GET", "POST"]
     }
 });
